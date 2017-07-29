42703fd6da55c136f4ea1996d70b80aaa57c3a02
Logicon211 <logicon211@gmail.com>
2017-07-29 12:35:03 -0300

    Fixed some shit

--- LudumDare39/Assets/Scripts/bullet.cs
+++ LudumDare39/Assets/Scripts/bullet.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class bullet : MonoBehaviour {
+public class bullet : MonoBehaviour, IProjectile {
+
+	int damageToTile = 1;
+	int damageToEnemies = 1;
+	int damageToPlayer = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +17,28 @@ public class bullet : MonoBehaviour {
 	
 	}
 
+	public void OnActorHit() {
+		// DO damage to them or something
+	}
+
 	void OnCollisionEnter2D (Collision2D col) 
 	{
 		if(col.gameObject.GetComponent<HealthController>() != null) {
 			Debug.Log(col.gameObject.GetComponent<HealthController>().health);
 			col.gameObject.GetComponent<HealthController>().takeDamage(1);
 		}
+	}
+
+	public void OnMapTileHit (MapTile mapTile) {
+		Level level = mapTile.level;
+		//Start destroying a radius
+
+		level.mapTiles [mapTile.x, mapTile.y].TakeDamage (damageToTile);
+
+		//refresh colliders this is probably to slow to put in every hit
+		level.refreshCollidersOnOuterTiles();
+
+		//Destory the projectile
+		Destroy (this.gameObject);
 	}
 }
