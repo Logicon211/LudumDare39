66ce740d2f5d99ffb3a430d15c3f2bf718379937
Jack Thomas <jackt@sfu.ca>
2017-07-29 13:19:12 -0700

    More fixins for barrel and mine

---
+++ LudumDare39/Assets/Scripts/BarrelExplodeOnImpact.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelExplodeOnImpact : MonoBehaviour {
+
+    public GameObject deathEffect;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //This isn't calling for some reason
+        //Test for projectile collision
+
+        if (col.gameObject.tag == "Rocket")
+        {
+            IProjectile projectile = (IProjectile)col.gameObject.GetComponent(typeof(IProjectile));
+            if (projectile != null)
+            {
+                projectile.OnActorHit();
+
+                    Instantiate(deathEffect, this.transform.position, Quaternion.identity);
+                    Destroy(this.gameObject);
+              
+            }
+
+        }
+        //Check for player collision, blow him up and he loses or whatever
+        //		if (col.gameObject.tag == "Player")
+        //		{
+        //			ship = col.gameObject.GetComponent<Ship>();
+        //			ship.takeHit();
+        //			kill();
+        //
+        //		}
+    }
+}
---
+++ LudumDare39/Assets/Scripts/MineExplodeOnContact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineExplodeOnContact : MonoBehaviour {
+    private GameObject player;
+    private float playerDist;
+    private Rigidbody2D RB;
+    public GameObject deathEffect;
+    public float triggerRadius = 3.0f;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        RB = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        playerDist = Vector3.Distance(RB.transform.position, (player.transform.position));
+        Debug.Log(playerDist);
+        if (playerDist < triggerRadius)
+        {
+            Instantiate(deathEffect, this.transform.position, Quaternion.identity);
+            Unit scriptin = player.GetComponent<Unit>();
+            scriptin.playerHealthChange(-10);
+            Destroy(this.gameObject);
+        }
+    }
+}
