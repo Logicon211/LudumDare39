911a2c59e6aceb1cad2227522ae057162282d337
Logicon211 <logicon211@gmail.com>
2017-07-30 18:24:48 -0300

    Trying to get persistent stuff saved between levels

---
+++ LudumDare39/Assets/PersistentGameObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentGameObject : MonoBehaviour {
+
+	private int playerLevel = 1;
+	private int playerWeapon = 0;
+
+	private static PersistentGameObject _instance;
+	// Use this for initialization
+	void Start () {
+		//DontDestroyOnLoad(transform.gameObject);
+
+		//if we don't have an [_instance] set yet
+		if (!_instance) {
+			_instance = this;
+			playerLevel = 1;
+			playerWeapon = 0;
+			Debug.Log ("creating instance of persistent object");
+		}
+		//otherwise, if we do, kill this thing
+		else {
+			Debug.Log ("destroying extra instance");
+			Destroy (this.gameObject);
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+		Debug.Log ("Current level: " + playerLevel);
+		Debug.Log ("Current weapon: " + playerWeapon);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public int getPlayerLevel(){
+		return playerLevel;
+	}
+
+	public void setPlayerLevel(int level) {
+		playerLevel = level;
+	}
+
+	public int getPlayerWeapon() {
+		return playerWeapon;
+	}
+
+	public void setPlayerWeapon(int weapon) {
+		playerWeapon = weapon;
+	}
+}
